ada185c6772880eafdba1cd3f9975c9c3229b792
falcon <aron.fiechter@gmail.com>
2018-12-14 15:44:28 +0100

    Remove unused imports

--- app/src/main/java/usi/sensorship/UI/fragments/PreferencesFragment.java
+++ app/src/main/java/usi/sensorship/UI/fragments/PreferencesFragment.java
@@ -3,9 +3,6 @@ package usi.sensorship.UI.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.preference.PreferenceFragmentCompat;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 
 import usi.sensorship.R;
