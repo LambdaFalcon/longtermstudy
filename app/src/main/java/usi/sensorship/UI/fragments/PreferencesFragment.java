414e34227bf411fe0962179a3bae480e452df8df
falcon <aron.fiechter@gmail.com>
2018-12-14 14:56:31 +0100

    Create PreferencesFragment with accompanying view and preferences.xml

---
+++ app/src/main/java/usi/sensorship/UI/fragments/PreferencesFragment.java
@@ -0,0 +1,40 @@
+package usi.sensorship.UI.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import usi.sensorship.R;
+
+/**
+ * A simple {@link Fragment} subclass implementing app preferences.
+ */
+public class PreferencesFragment extends PreferenceFragmentCompat {
+
+    public PreferencesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_preferences, container, false);
+    }
+
+
+}
